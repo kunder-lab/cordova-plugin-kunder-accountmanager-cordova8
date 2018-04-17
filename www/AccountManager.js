af212097aac1bb4b7e1929fd06e9eea22a4d8a95
Quentin Fung <31120177+QuentinFung@users.noreply.github.com>
2018-04-17 11:39:45 +0800

    update the class name

--- www/AccountManager.js
+++ www/AccountManager.js
@@ -3,31 +3,31 @@
 module.exports = (function() {
 
   var _initWithKey = function(encryptionKey, successCallback, errorCallback) {
-    cordova.exec(successCallback, errorCallback, 'AccountManagerPlugin', 'initWithKey', [encryptionKey]);
+    cordova.exec(successCallback, errorCallback, 'AccountManager', 'initWithKey', [encryptionKey]);
   };
 
   var _addAccount = function(accountName, token, accountType, group, userData, successCallback, errorCallback) {
-    cordova.exec(successCallback, errorCallback, 'AccountManagerPlugin', 'addAccount', [accountName, token, accountType, group, userData]);
+    cordova.exec(successCallback, errorCallback, 'AccountManager', 'addAccount', [accountName, token, accountType, group, userData]);
   };
 
   var _removeAccount = function(accountType, scope, successCallback, errorCallback) {
-    cordova.exec(successCallback, errorCallback, 'AccountManagerPlugin', 'removeAccount', [accountType, scope]);
+    cordova.exec(successCallback, errorCallback, 'AccountManager', 'removeAccount', [accountType, scope]);
   };
 
   var _getUserAccount = function(accountType, group, scope, successCallback, errorCallback){
-    cordova.exec(successCallback, errorCallback, 'AccountManagerPlugin', 'getUserAccount', [accountType, group, scope]);
+    cordova.exec(successCallback, errorCallback, 'AccountManager', 'getUserAccount', [accountType, group, scope]);
   };
 
   var _getPassword = function(accountType, group, scope, successCallback, errorCallback) {
-    cordova.exec(successCallback, errorCallback, 'AccountManagerPlugin', 'getPassword', [accountType, group, scope]);
+    cordova.exec(successCallback, errorCallback, 'AccountManager', 'getPassword', [accountType, group, scope]);
   };
 
   var _setUserData = function(accountType, group, data, successCallback, errorCallback) {
-    cordova.exec(successCallback, errorCallback, 'AccountManagerPlugin', 'setUserData', [accountType, group, data]);
+    cordova.exec(successCallback, errorCallback, 'AccountManager', 'setUserData', [accountType, group, data]);
   };
 
   var _changePassword = function(accountType, group, scope, newPassword, successCallback, errorCallback) {
-    cordova.exec(successCallback, errorCallback, 'AccountManagerPlugin', 'changePassword', [accountType, group, scope, newPassword]);
+    cordova.exec(successCallback, errorCallback, 'AccountManager', 'changePassword', [accountType, group, scope, newPassword]);
   };
 
   return {
